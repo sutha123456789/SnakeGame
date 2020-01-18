49b713029d084df0d5080a87d53805b4c53115f0
sutha <sutha@LAPTOP-1VNDFP0B>
2020-01-18 17:51:03 +0700

    test2

---
+++ Chili Framework 2016/Engine/Board.cpp
@@ -0,0 +1,26 @@
+#include "Board.h"
+#include<assert.h>
+
+Board::Board(Graphics& gfx)
+	:
+	gfx(gfx)
+{}
+
+void Board::DrawCell(Location & loc, Color c)
+{
+	assert(loc.x >= 0);
+	assert(loc.x < width);
+	assert(loc.y >= 0);
+	assert(loc.y < height);
+	gfx.DrawRectDim(loc.x * dimentsion, loc.y * dimentsion,dimentsion,dimentsion,c);
+}
+
+int Board::GetGridWidth() const
+{
+	return width;
+}
+
+int Board::GetGridHeight() const
+{
+	return height;
+}
---
+++ Chili Framework 2016/Engine/Snake.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include"Board.h"
+
+
+
+class Snake
+{
+private:
+	class Segment
+	{
+	private:
+		Location loc;
+		Color c;
+
+	};
+private :
+	Segment segment[100];
+
+};
